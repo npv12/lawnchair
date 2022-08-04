b701e63e07f3a97c577b1375a18b5ad4f8be41c3
jhonboy121 <alfredmathew05@gmail.com>
2022-08-04 22:07:50 +0530

    quickstep: SystemUiProxy: add a null check on getRecentTask(*) method
    Stacktrace:
    
    E AndroidRuntime: Process: com.android.launcher3
    E AndroidRuntime: java.lang.NullPointerException
    E AndroidRuntime: 	at java.util.Objects.requireNonNull(Objects.java:220)
    E AndroidRuntime: 	at java.util.Arrays$ArrayList.<init>(Arrays.java:3738)
    E AndroidRuntime: 	at java.util.Arrays.asList(Arrays.java:3725)
    E AndroidRuntime: 	at com.android.quickstep.SystemUiProxy.getRecentTasks(SystemUiProxy.java:763)
    E AndroidRuntime: 	at com.android.quickstep.RecentTasksList.loadTasksInBackground(RecentTasksList.java:164)
    E AndroidRuntime: 	at com.android.quickstep.RecentTasksList.lambda$getTasks$4$RecentTasksList(RecentTasksList.java:124)
    E AndroidRuntime: 	at com.android.quickstep.RecentTasksList$$ExternalSyntheticLambda2.run(Unknown Source:8)
    E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:938)
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- quickstep/src/com/android/quickstep/SystemUiProxy.java
+++ quickstep/src/com/android/quickstep/SystemUiProxy.java
@@ -794,8 +794,11 @@ public class SystemUiProxy implements ISystemUiProxy,
     public ArrayList<GroupedRecentTaskInfo> getRecentTasks(int numTasks, int userId) {
         if (mRecentTasks != null) {
             try {
-                return new ArrayList<>(Arrays.asList(mRecentTasks.getRecentTasks(numTasks,
-                        RECENT_IGNORE_UNAVAILABLE, userId)));
+                final GroupedRecentTaskInfo[] recentTasksList = mRecentTasks.getRecentTasks(numTasks,
+                    RECENT_IGNORE_UNAVAILABLE, userId);
+                if (recentTasksList != null) {
+                    return new ArrayList<>(Arrays.asList(recentTasksList));
+                }
             } catch (RemoteException e) {
                 Log.w(TAG, "Failed call getRecentTasks", e);
             }
